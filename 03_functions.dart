434c3da6923f7b595846a280efb925efef72ae55
chenhj59 <128605551+chenhj59@users.noreply.github.com>
2023-03-27 19:45:54 +0800

    Update 03_functions.dart

--- 03_functions.dart
+++ 03_functions.dart
@@ -1,3 +1,5 @@
+import '01_variables.dart';
+
 int fibonacci(int n) {
   if (n == 0 || n == 1) return n;
   return fibonacci(n - 1) + fibonacci(n - 2);
@@ -5,4 +7,6 @@ int fibonacci(int n) {
 
 void main() {
   var result = fibonacci(20);
+  
+  flybyObjects.where((name)=>name.contains('turn')).forEach(print);
 }
