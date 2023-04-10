f7f3b44456c2c83427a6160851c193a3725629ba
陈 豪嘉 <chenhj@mail.ynu.edu.cn>
2023-04-10 02:51:30 +0000

    05

--- 05_enums.dart
+++ 05_enums.dart
@@ -3,7 +3,8 @@ enum PlanetType { terrestrial, gas, ice }
 /// Enum that enumerates the  different planets in our solar system
 /// and some of their properties
 enum Planet {
-  earth(hasRings: false, planetType: PlanetType.terrestrial, moons: 1), // 位置错位也可行
+  earth(
+      hasRings: false, planetType: PlanetType.terrestrial, moons: 1), // 位置错位也可行
   mercury(planetType: PlanetType.terrestrial, moons: 0, hasRings: false),
   venus(planetType: PlanetType.terrestrial, moons: 0, hasRings: false),
   // ...
@@ -12,8 +13,10 @@ enum Planet {
 
   // A constant generating constructor
   const Planet(
-    // {}：字典映射// requried：要求必须有
-      {required this.planetType, required this.moons, required this.hasRings}); 
+      // {}：字典映射; requried：要求必须有
+      {required this.planetType,
+      required this.moons,
+      required this.hasRings});
 
   /// All instance variables are final
   final PlanetType planetType;
@@ -27,7 +30,7 @@ enum Planet {
 
 void main() {
   final yourPlanet = Planet.earth;
-  
+
   if (!yourPlanet.isGiant) {
     print('Your planet is not a "giant planet".');
   }
